04c8b6291dfd6c00784c7167b82ba22f73fa8252
Maksin K <maks4tmk5423@yandex.ru>
2023-08-23 23:13:21 +0300

    initial commit

---
+++ Задача 1. Сравнения в дробях/Задача 1. Сравнения в дробях.cpp
@@ -0,0 +1,39 @@
+﻿// Задача 1. Сравнения в дробях.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+
+	bool operator == (const Fraction& other) { return this->numerator_ == other.numerator_ && this->denominator_ == other.denominator_; }
+	bool operator != (const Fraction& other) { return this->numerator_ != other.numerator_ && this->denominator_ != other.denominator_; }
+	bool operator < (const Fraction& other) { return this->numerator_ > other.numerator_ && this->denominator_ > other.denominator_; }
+	bool operator > (const Fraction& other) { return this->numerator_ < other.numerator_ && this->denominator_ < other.denominator_; }
+	bool operator <= (const Fraction& other) { return this->numerator_ >= other.numerator_ && this->denominator_ >= other.denominator_; }
+	bool operator >= (const Fraction& other) { return this->numerator_ <= other.numerator_ && this->denominator_ <= other.denominator_; }
+};
+
+int main()
+{
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
+	return 0;
+}
